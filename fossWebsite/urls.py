aacc2fc23df3782a242c951f5ee2185f1928c940
Chirath02 <chirath.02@gmail.com>
2017-01-30 07:26:38 +0530

    Fixed url.py issues

--- fossWebsite/urls.py
+++ fossWebsite/urls.py
@@ -5,11 +5,11 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^search', views.search),
-    url(r'^contact', views.contact),
-    url(r'^register', include('register.urls')),
-    url(r'^achievement', include('achievement.urls')),
-    url(r'^images', include('images.urls')),
-    url(r'^admin', include(admin.site.urls)),
+    url(r'^search/', views.search),
+    url(r'^contact/', views.contact),
+    url(r'^register/', include('register.urls')),
+    url(r'^achievement/', include('achievement.urls')),
+    url(r'^images/', include('images.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
 
