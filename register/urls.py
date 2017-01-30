b88a9e9dab7294362edd52078beed5b633ed6ada
Chirath02 <chirath.02@gmail.com>
2017-01-30 06:29:41 +0530

    Fixed warnings on url for begining witg '/'

--- register/urls.py
+++ register/urls.py
@@ -3,12 +3,12 @@ from register import views
 
 
 urlpatterns = [
-    url(r'^/login/$', views.login),
-    url(r'^/forpass/$', views.forpass),
-    url(r'^/logout/$', views.logout),
-    url(r'^/new/$', views.newregister),
-    url(r'^/mypage/$', views.mypage),
-    url(r'^/profile/(?P<user_name>\w+)/$',views.profile),
-    url(r'^/update_profile/$',views.update_profile),
-    url(r'^/update_profile_pic/$',views.update_profile_pic),
+    url(r'^login/$', views.login),
+    url(r'^forpass/$', views.forpass),
+    url(r'^logout/$', views.logout),
+    url(r'^new/$', views.newregister),
+    url(r'^mypage/$', views.mypage),
+    url(r'^profile/(?P<user_name>\w+)/$',views.profile),
+    url(r'^update_profile/$',views.update_profile),
+    url(r'^update_profile_pic/$',views.update_profile_pic),
 ]
