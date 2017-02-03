12c07f1dd288601368d6c1dca720939e68a5bad4
Chirath02 <chirath.02@gmail.com>
2017-02-03 13:28:55 +0530

    login before accesing proposal

--- fossProposal/views.py
+++ fossProposal/views.py
@@ -1,18 +1,29 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 
 from fossWebsite.helper import get_session_variables
 from .models import Proposal
 from register.models import User_info
+from fossWebsite.helper import logged_in
 
 # Create your views here.
 
 class ProposalListView(ListView):
-
     model = Proposal
 
 
 class ProposalCreateView(CreateView):
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        self.object_list = Proposal.objects.all()
+        context = self.get_context_data(object_list=self.object_list)
+        is_loggedin, username = get_session_variables(self.request)
+        if is_loggedin is False:
+            print is_loggedin
+            return redirect('login')
+        return self.render_to_response(context)
 
     model = Proposal
     fields = '__all__'
