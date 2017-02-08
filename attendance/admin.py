3f925cc37bf237b303c11894aea08c34dda4a79c
Chirath02 <chirath.02@gmail.com>
2017-02-08 19:41:32 +0530

    rest now works

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from attendance.models import Attendance
 
-# Register your models here.
+admin.site.register(Attendance)
--- attendance/urls.py
+++ attendance/urls.py
@@ -5,6 +5,8 @@ from .views import AttendanceDetail, AttendanceList
 urlpatterns = [
     url(r'^$', AttendanceList.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', AttendanceDetail.as_view()),
+
     # browsable api django-rest framework
+
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
