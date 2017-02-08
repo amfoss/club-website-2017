70cb3c4555690e9ffb36f0829cc30f8144cd68f3
Chirath02 <chirath.02@gmail.com>
2017-02-08 18:45:41 +0530

    added attendace db

--- attendance/models.py
+++ attendance/models.py
@@ -1,5 +1,25 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from register.models import User_info
+
+
+class Attendance(models.Model):
+
+    # student details
+    username = models.ForeignKey(User_info, on_delete=models.CASCADE)
+
+    # date and time added
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # added by
+    added_by = models.ForeignKey(User_info, on_delete=models.CASCADE, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('attendance-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.username
