26b7b0f0dce0fe98165ad81525d01ffed78fd783
Chirath02 <chirath.02@gmail.com>
2017-02-08 18:38:21 +0530

    Changed some fields in proposals to optional

--- fossProposal/models.py
+++ fossProposal/models.py
@@ -15,30 +15,30 @@ class Proposal(models.Model):
     topic = models.CharField(max_length=300)
     speakers = models.CharField(max_length=500)
     nat_or_inter = models.CharField(max_length=500)
-    trainer_bio = models.TextField()
+    trainer_bio = models.TextField(blank=True, null=True)
     coordinators = models.CharField(max_length=400)
     level = models.CharField(max_length=200)
     expected_no_of_participants = models.CharField(max_length=200)
 
     # prerequisites
-    prerequisites_software = models.TextField()
-    prerequisites_hardware = models.TextField()
-    prerequisites_knowledge = models.TextField()
+    prerequisites_software = models.TextField(blank=True, null=True)
+    prerequisites_hardware = models.TextField(blank=True, null=True)
+    prerequisites_knowledge = models.TextField(blank=True, null=True)
 
     # course
-    course_plan = models.TextField()
-    student_short_list = models.TextField()
+    course_plan = models.TextField(blank=True, null=True)
+    student_short_list = models.TextField(blank=True, null=True)
 
     # financial
-    travel = models.CharField(max_length=200)
-    trainer_fee = models.CharField(max_length=200)
-    accommodation = models.CharField(max_length=200)
-    transportation = models.CharField(max_length=200)
-    other = models.CharField(max_length=200)
+    travel = models.CharField(max_length=200, blank=True, null=True)
+    trainer_fee = models.CharField(max_length=200, blank=True, null=True)
+    accommodation = models.CharField(max_length=200, blank=True, null=True)
+    transportation = models.CharField(max_length=200, blank=True, null=True)
+    other = models.CharField(max_length=200, blank=True, null=True)
 
     # lab requirements
     lab_requirements = models.CharField(max_length=200)
-    icts_support = models.TextField()
+    icts_support = models.TextField(blank=True, null=True)
 
     # permissions
     permisions = models.TextField()
