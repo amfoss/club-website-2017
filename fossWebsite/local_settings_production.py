55e473409aa64ce39f65cfe532fe649b82b64843
Chirath R <chirath@ubuntu.(none)>
2017-02-07 21:58:59 -0800

    Added new apacheconf for ubuntu 12

--- fossWebsite/local_settings_production.py
+++ fossWebsite/local_settings_production.py
@@ -4,24 +4,22 @@ import os
 # Move this file to the same folder as main settings.py file
 # Rename the file as local_settings.py
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+DEBUG = True
+
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ADMINS = (
     ('Seshagiri Prabhu', 'seshagiriprabhu@gmail.com'),
-    ('Tony Thomas', '01tonythomas@gmail.com' ),
-    ('Bithin A', 'bithin2007@gmail.com'),
-    ('FOSS@Amrita', 'fossatamrita@gmail.com'),
 )
 
 ADMINS_EMAIL = map(lambda x: x[1], ADMINS)
 
 # Sending email using SMTP gmail server
 
-EMAIL_HOST_USER = 'fossatamrita@gmail.com'
-EMAIL_HOST_PASSWORD = 'batmanrobin123'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST_USER = 'amritapurifoss@gmail.com'
+#EMAIL_HOST_PASSWORD = '@mritaf0$$)(*'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # If running in debug mode, write emails to files.
@@ -31,8 +29,8 @@ if DEBUG:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['foss.amrita.ac.in', 'amritafoss.in', '10.0.0.79', '122.15.144.133', 'localhost', 'www.amritafoss.in' ]
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
+
 # Generate a secret key, and don't share it with anybody.
 # from django.utils.crypto import get_random_string
 # chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
@@ -53,20 +51,21 @@ PASSWORD_HASHERS = (
 )
 
 # To be changed with site specific values
-RECAPTCHA_PUBLIC_KEY = '6LemMxATAAAAAG8qyflYeJoJ5FBL48w6eqsZPNx6'
-RECAPTCHA_PRIVATE_KEY = '6LemMxATAAAAAKV5uEx7DsfZM-eeepV3MflNJZNN'
+RECAPTCHA_PUBLIC_KEY = '6LfcYvkSAAAAAAW8UHVHw3oBuF7S5NJZ-jnBUxn1'
+RECAPTCHA_PRIVATE_KEY = '6LfcYvkSAAAAAOkXjirru1nTiwvwi_drX93sTQKo'
 RECAPTCHA_USE_SSL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'fossamrita',                         # Or path to database file if using sqlite3.
-        'USER': 'fossadmin',                    # Not used with sqlite3.
-        'PASSWORD': 'adminmysql',                   # Not used with sqlite3.
-        'HOST': '10.0.0.79',                              # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                              # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db/db.db',                         # Or path to database file if using sqlite3.
+        'USER': '',                    # Not used with sqlite3.
+        'PASSWORD': '',                   # Not used with sqlite3.
+        'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                              # Set to empty string for default. Not used with sqlite3.
     }
 }
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
