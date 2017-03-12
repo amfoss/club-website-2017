fd6d6429c8656db2d1c67f06647b31dcb1664fbb
Chirath02 <chirath.02@gmail.com>
2017-03-13 00:00:56 +0530

    url issues fixed

--- register/urls.py
+++ register/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import *
 from register import views
-from register.views import UpdateProfileView, ProfileDetailView, StudentDetailView, StudentCreateView
+from register.views import UpdateProfileView, ProfileDetailView, StudentDetailView, StudentCreateView, \
+    UpdateStudentDetailView
 
 urlpatterns = [
     url(r'^mypage/$', views.mypage, name='my-page'),
+    url(r'^profile/$', ProfileDetailView.as_view(), name='profile'),
     url(r'^profile/(?P<slug>[\w-]+)/$', ProfileDetailView.as_view(), name='profile'),
     url(r'^update/profile$', UpdateProfileView.as_view(), name='profile-update'),
     url(r'^update/profile/pic/$', views.update_profile_pic, name='profile-pic-update'),
 
+    url(r'^student/profile/$', StudentDetailView.as_view(), name='student-data'),
     url(r'^student/profile/(?P<slug>[\w-]+)/$', StudentDetailView.as_view(), name='student-data'),
     url(r'^student/add/profile/$', StudentCreateView.as_view(), name='student-data'),
+    url(r'^student/update/profile/$', UpdateStudentDetailView.as_view(), name='update-student-data'),
 ]
--- register/views.py
+++ register/views.py
@@ -1,9 +1,11 @@
 # Django libraries
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext as _
 
 from register.forms import UpdateProfileForm, PasswordResetForm, RegistrationForm
 from achievement.models import *
@@ -58,6 +60,38 @@ class ProfileDetailView(DetailView):
 
     slug_field = 'username'
 
+    def get_object(self, queryset=None):
+
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+
+        if slug is None:
+            slug = self.request.user.username
+
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError("Generic detail view %s must be called with "
+                                 "either an object pk or a slug."
+                                 % self.__class__.__name__)
+
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+        return obj
+
 
 class UpdateProfileView(LoginRequiredMixin, UpdateView):
     template_name = 'registration/update_profile.html'
@@ -82,6 +116,52 @@ class StudentDetailView(DetailView):
               'comments', ]
 
     slug_field = 'username'
+
+    def get_object(self, queryset=None):
+
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+
+        if slug is None:
+            slug = self.request.user.username
+
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError("Generic detail view %s must be called with "
+                                 "either an object pk or a slug."
+                                 % self.__class__.__name__)
+
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+        return obj
+
+
+class UpdateStudentDetailView(LoginRequiredMixin, UpdateView):
+    template_name = 'registration/update_profile.html'
+    model = Student
+    fields = ['roll_number', 'branch', 'year', 'cgpa', 'mentors', 'system_number', 'responsibility1',
+              'responsibility2', 'responsibility3', 'responsibility4', 'responsibility5', 'responsibility_count',
+              'comments', ]
+
+    success_message = 'Your settings have been saved.'
+    success_url = '/register/student/profile/'
+
+    def get_object(self):
+        return Student.objects.get(username=self.request.user.username)
 
 
 class StudentCreateView(CreateView):
