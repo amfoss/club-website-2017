f3dfa8828d434f4d7781fccc466d6a2998403519
Chirath02 <chirath.02@gmail.com>
2017-03-14 00:08:52 +0530

    fixed registration and removed lots of unwanted code

--- fossWebsite/forms.py
+++ fossWebsite/forms.py
@@ -1,10 +1,51 @@
 from django import forms
 from captcha.fields import ReCaptchaField
+from django.contrib.auth import get_user_model
+from registration.forms import RegistrationForm
 
 
-# Write to us (Contact field)
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=200)
     email = forms.EmailField()
     message = forms.CharField(max_length=1500)
     captcha = ReCaptchaField()
+
+
+class CustomRegisterUserForm(RegistrationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
+
+    email = forms.EmailField(
+        required=True,
+        label='Email',
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Email Address'}
+        )
+    )
+
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Username'}
+        )
+    )
+
+    password1 = forms.CharField(
+        required=True,
+        max_length=100,
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Password'}
+        )
+    )
+
+    password2 = forms.CharField(
+        max_length=100,
+        required=True,
+        label='Re-enter password',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Re Enter Your Password'}
+        ),
+    )
--- register/views.py
+++ register/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext as _
 
-from register.forms import UpdateProfileForm, PasswordResetForm, RegistrationForm
 from achievement.models import *
 from images.models import ProfileImage
 from fossWebsite.helper import error_key
@@ -15,39 +14,10 @@ from fossWebsite.helper import get_session_variables
 
 # Python libraries
 from django.views.generic import *
-from django.contrib.auth.forms import PasswordResetForm
-from django.shortcuts import redirect
 
 # generic views
 from django.views.generic import CreateView, UpdateView
-
 from register.models import Student
-
-
-class RegistrationView(CreateView):
-    form_class = RegistrationForm
-    model = User_info
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.set_password(User_info.objects.make_random_password())
-        obj.save()
-
-        # This form only requires the "email" field, so will validate.
-        reset_form = PasswordResetForm(self.request.POST)
-        reset_form.is_valid()  # Must trigger validation
-        # Copied from django/contrib/auth/views.py : password_reset
-        opts = {
-            'use_https': self.request.is_secure(),
-            'email_template_name': 'registration/verification.html',
-            'subject_template_name': 'registration/verification_subject.txt',
-            'request': self.request,
-            # 'html_email_template_name': provide an HTML content template if you desire.
-        }
-        # This form sends the email on save()
-        reset_form.save(**opts)
-
-        return redirect('accounts:register-done')
 
 
 class ProfileDetailView(DetailView):
