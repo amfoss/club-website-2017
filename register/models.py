1bb23cf766f6692bad61b75f93d5ca69a130e3d2
Chirath02 <chirath.02@gmail.com>
2017-03-13 19:15:05 +0530

    changed migrations

--- register/models.py
+++ register/models.py
@@ -28,19 +28,8 @@ class User_info(AbstractUser):
     expertise = models.CharField(max_length=200, blank=True, null=True)
     goal = models.CharField(max_length=15, choices=GOAL_CHOICES, blank=True, null=True)
 
-    username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
-
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        unique=True,
-        primary_key=True,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
-    )
+    username = models.CharField(max_length=20, unique=True, blank=False, primary_key=True)
+ 
     # email = models.EmailField(blank=False, unique=True)
     # password = models.CharField(max_length=255, blank=False)
     # last_login = models.DateTimeField(auto_now=True)
